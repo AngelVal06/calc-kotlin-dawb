aad2b9fb84e21127b013350a447097015bbc8842
avalrom607 <avalrom607@g.educaand.es>
2025-03-20 19:20:22 +0100

    Función suma

--- Calc.kt
+++ Calc.kt
@@ -1,11 +1,15 @@
+
+
 fun main(args: Array) {
     val (operation, num1, num2) = parseArguments(args)
     val result = when (operation) {
         "multiplica" -> multiplica(num1, num2)
+        "suma" -> suma(num1, num2)
         else -> throw IllegalArgumentException("Operación no soportada")
     }
     showResult(operation, result)
 }
+
 
 fun parseArguments(args: Array): Triple {
     if (args.size != 3) {
@@ -23,4 +27,8 @@ fun multiplica(num1: Int, num2: Int): Int {
 
 fun showResult(operation: String, result: Int) {
     println("El resultado de la operación \"$operation\" es: $result")
+}
+
+fun suma(num1: Int, num2: Int): Int {
+    return num1 + num2
 }
